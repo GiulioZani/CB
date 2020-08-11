a1b534b9f33828cf8de0f36c0f26433259e5e933
GiulioZani <giulio.zani@gmail.com>
2020-08-11 12:30:16 +0200

    all

---
+++ maze/maze.py
@@ -0,0 +1,69 @@
+class Maze:
+    def __init__(self, nome_file):
+        f = open(nome_file)
+        data_grezzi = f.read()
+        lines = data_grezzi.split('\n')[:-1]
+        self.adjacents = {}
+        for i in range(len(lines)):
+            line = lines[i]
+            for j in range(len(line)):
+                adiacenti = []
+                self.adjacents[(i,j)] = adiacenti
+                if lines[i][j] == ' ':
+                    if j-1 >= 0 and lines[i][j-1] == ' ':
+                        adiacenti.append((i, j-1))
+                    if j+1 < len(lines[i]) and lines[i][j+1] == ' ':
+                        adiacenti.append((i, j+1))
+                    if i-1 >= 0 and lines[i-1][j] == ' ':
+                        adiacenti.append((i-1, j))
+                    if i < len(lines)-1 and lines[i+1][j] == ' ':
+                        adiacenti.append((i+1, j))
+
+        self.directions = {}
+        for i in range(len(lines)):
+            for j in range(len(lines[i])):
+                if lines[i][j] == ' ':
+                    self.directions[(i, j)] = '?'
+                    if j == 0:
+                        self.directions[(i, j)] = 'W'
+                    if j == len(lines[i]) - 1:
+                        self.directions[(i, j)] = 'E'
+                    if i == 0:
+                        self.directions[(i, j)] = 'N'
+                    if i == len(lines)-1:
+                        self.directions[(i, j)] = 'S'
+
+        print(self.directions)
+
+    def __str__(self):
+        pass
+
+    def help(self):
+        done = False
+        while not done:
+            done = True
+            for corridor in self.directions:
+                if self.directions[corridor] == '?':
+                    adjacents_positions = self.adjacents[corridor]
+                    for position in adjacents_positions:
+                        if self.directions[position] != '?':
+                            if corridor[0] == position[0] + 1:
+                                self.directions[corridor] = 'N'
+                            if corridor[0] == position[0] - 1:
+                                self.directions[corridor] = 'S'
+                            if corridor[1] == position[1] - 1:
+                                self.directions[corridor] = 'W'
+                            if corridor[1] == position[1] + 1:
+                                self.directions[corridor] = 'E'
+                            done = False
+                            break
+
+
+
+def main():
+    maze = Maze('data.txt')
+    maze.help()
+    print(maze.directions)
+
+if __name__ == '__main__':
+    main()
